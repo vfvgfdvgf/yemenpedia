e2780b9b2c59cd033b8b0b26d49b6647b01ada7a
vfvgfdvgf <halax.7y7@gmail.com>
2025-08-12 13:07:59 +0300

    urls.py

--- articles/urls.py
+++ articles/urls.py
@@ -1,20 +1,18 @@
-from django.urls import path
-from . import views
 from django.urls import path, include
+from . import views
+from django.conf import settings             # استيراد settings
+from django.conf.urls.static import static   # استيراد static
 
 app_name = 'articles'
 
 urlpatterns = [
     path('', views.article_list_view, name='article_list'),
 
-    # رابط لعرض تفاصيل المقالة بـ pk فقط
     path('article/<int:pk>/', views.article_detail_view, name='article_detail'),
 
-    # رابط لعرض تفاصيل المقالة بـ pk و slug (لتحسين SEO)
     path('article/<int:pk>/<slug:slug>/', views.article_detail_view, name='article_detail'),
 
-      path('dashboard/', include('dashboard.urls')),
-
+    path('dashboard/', include('dashboard.urls')),
 
     path('privacy-policy/', views.privacy_policy_view, name='privacy_policy'),
     path('categories/', views.categories_list_view, name='categories_list'),
@@ -23,3 +21,6 @@ urlpatterns = [
     path('faq/', views.faq_view, name='faq'),
     path('category/<slug:slug>/', views.category_article_list_view, name='category_articles'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
